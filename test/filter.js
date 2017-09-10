1c0251b723533e2e761f999ca5f31b8ec373cd59
Jacob Tschirhart <jacobtshirt@gmail.com>
2017-09-09 19:49:29 -0500

    tests

--- test/filter.js
+++ test/filter.js
@@ -1,10 +1,31 @@
+import assert from 'assert';
+import sinon from 'sinon';
+import filter from '../';
+
 describe('Filter Spec', () => {
   describe('#filter', () => {
-    it.skip('should call next function if method is allowed', () => {
-
+    const nextSpy = sinon.spy();
+    const sendStatusSpy = sinon.spy();
+    afterEach(() => {
+      nextSpy.reset();
+      sendStatusSpy.reset();
+    })
+    it('should call next function if method is allowed', () => {
+      const instance = filter(['POST']);
+      const req = { method: 'POST'  };
+      const res = { sendStatus: sendStatusSpy };
+      instance(req, res, nextSpy);
+      assert.ok(nextSpy.calledOnce);
+      assert.ok(sendStatusSpy.notCalled);
     });
-    it.skip('should send HTTP status code 406 if method is not allowed', () => {
-
+    it('should send HTTP status code 406 if method is not allowed', () => {
+      const instance = filter(['POST']);
+      const req = { method: 'GET'  };
+      const res = { sendStatus: sendStatusSpy };
+      instance(req, res, nextSpy);
+      assert.ok(nextSpy.notCalled);
+      assert.ok(sendStatusSpy.calledOnce);
+      assert.ok(sendStatusSpy.calledWithExactly(405));
     });
   });
 });
