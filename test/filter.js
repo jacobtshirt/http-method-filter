0c2639c585e35b1c4cd47b3b47d9b1aa05a5be72
Tschirhart, Jacob L <JTSCHIRH@amfam.com>
2017-09-09 19:35:37 -0500

    define test suite

---
+++ test/filter.js
@@ -0,0 +1,10 @@
+describe('Filter Spec', () => {
+  describe('#filter', () => {
+    it.skip('should call next function if method is allowed', () => {
+
+    });
+    it.skip('should send HTTP status code 406 if method is not allowed', () => {
+
+    });
+  });
+});
