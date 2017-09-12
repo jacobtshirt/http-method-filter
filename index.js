3627ccd4bc83972c4a394cc93a5984ae5fcd2db7
Jacob Tschirhart <jacobtshirt@gmail.com>
2017-09-12 12:18:24 -0500

    polyfill es6 exports quickfix

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports.default = filter;
+
 /**
   filter - rejects all methods not in whitelist
   calls next() callback if req.method is included in whitelist
@@ -5,7 +12,7 @@
   @param {Array<String>} whitelist array of allowed HTTP methods
   @param {Number} statusCode optional status code override (defaults to 405)
 */
-export default function filter(whitelist, statusCode) {
+function filter(whitelist, statusCode) {
   return (req, res, next) => {
     if (whitelist && whitelist.includes(req.method)) next();
     else res.sendStatus(statusCode || 405);
