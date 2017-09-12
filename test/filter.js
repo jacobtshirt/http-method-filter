b8c1673117e4a4b2978a8de0b4d58d41076cba66
Jacob Tschirhart <jacobtshirt@gmail.com>
2017-09-12 08:33:03 -0500

    bumping to version 1.0.0

--- test/filter.js
+++ test/filter.js
@@ -6,10 +6,12 @@ describe('Filter Spec', () => {
   describe('#filter', () => {
     const nextSpy = sinon.spy();
     const sendStatusSpy = sinon.spy();
+
     afterEach(() => {
       nextSpy.reset();
       sendStatusSpy.reset();
-    })
+    });
+
     it('should call next function if method is allowed', () => {
       const instance = filter(['POST']);
       const req = { method: 'POST'  };
@@ -18,7 +20,7 @@ describe('Filter Spec', () => {
       assert.ok(nextSpy.calledOnce);
       assert.ok(sendStatusSpy.notCalled);
     });
-    
+
     it('should send default HTTP status code 405 if method is not allowed', () => {
       const instance = filter(['POST']);
       const req = { method: 'GET'  };
