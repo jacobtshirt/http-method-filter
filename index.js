515de109b6e07427b13555827f1f1285637aa7ff
Jacob Tschirhart <jacobtshirt@gmail.com>
2017-09-12 08:27:41 -0500

    added jsdocs and check for truthy whitelist

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+/**
+  filter - rejects all methods not in whitelist
+  calls next() callback if req.method is included in whitelist
+
+  @param {Array<String>} whitelist array of allowed HTTP methods
+  @param {Number} statusCode optional status code override (defaults to 405)
+*/
 export default function filter(whitelist, statusCode) {
   return (req, res, next) => {
-    if (whitelist.includes(req.method)) next();
+    if (whitelist && whitelist.includes(req.method)) next();
     else res.sendStatus(statusCode || 405);
   };
 }
